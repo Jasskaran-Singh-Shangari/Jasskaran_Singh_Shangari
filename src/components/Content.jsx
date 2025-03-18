d0e82dfe4bb9b4bd32f2d5436a9d1767ab513b5c
Jasskaran Singh Shangari <jasskaransingh0007@gmail.com>
2025-03-18 10:15:50 +0530

    making images work

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -115,29 +115,29 @@ const Content = () => {
         <p>Are you looking for a passionate web developer to bring your ideas to life? I specialize in crafting innovative, visually engaging websites that leave a lasting impression. Whether it’s building dynamic user experiences, experimenting with creative animation styles, or collaborating on unique projects, I’m here to make your vision a reality. Let’s connect and discuss how I can contribute to your goals. Whether you have a specific project in mind, need assistance with web development, or simply want to chat about creative designs and cutting-edge animations, I’m always excited to collaborate and share ideas. Drop me a message, and I’ll get back to you as soon as possible. Together, we can create something extraordinary!
         </p>
         <ul className='list-none flex justify-center items-center space-x-10 '>
-          <li className='cursor-pointer grayscale hover:grayscale-0'><a href="https://www.instagram.com/jassi_sabka_bhai/#"><img src="src/assets/icons\insta.webp" alt="Instagram" width="100px" height="100px" onClick={redirect} /></a></li>
-          <li className='cursor-pointer grayscale hover:grayscale-0' onClick={handleClick1}><img src="src/assets/icons\whatsapp.png" alt="Whatsapp" width="80px" height="80px" /></li>
-          <li className='grayscale hover:grayscale-0'><a href="https://dev.to/jasskaran_singh_shangari"><img src="src/assets/icons\dev-to.png" alt="Dev.to" width="70px"height="80px" onClick={redirect} /></a></li>
-          <li className='grayscale hover:grayscale-0'><a href="https://www.linkedin.com/in/jasskaranshangari/"><img src="src/assets/icons\linked.webp" alt="LinkedIn" width="110px" onClick={redirect} /></a></li>
+          <li className='cursor-pointer grayscale hover:grayscale-0'><a href="https://www.instagram.com/jassi_sabka_bhai/#"><img src={assets.Insta} alt="Instagram" width="100px" height="100px" onClick={redirect} /></a></li>
+          <li className='cursor-pointer grayscale hover:grayscale-0' onClick={handleClick1}><img src={assets.Whatsapp} alt="Whatsapp" width="80px" height="80px" /></li>
+          <li className='grayscale hover:grayscale-0'><a href="https://dev.to/jasskaran_singh_shangari"><img src={assets.Devto} alt="Dev.to" width="70px"height="80px" onClick={redirect} /></a></li>
+          <li className='grayscale hover:grayscale-0'><a href="https://www.linkedin.com/in/jasskaranshangari/"><img src={assets.Linkedin} alt="LinkedIn" width="110px" onClick={redirect} /></a></li>
         </ul>
     </footer>
     <div className="Banner-image h-[50vh]" >
             <ul>
                 <li>
                     <span className="icon">
-                        <img src="src/assets/images/mail.png" alt="Mail" width="50px" height="50px" data-text="Mail"/>
+                        <img src={assets.Mail} alt="Mail" width="50px" height="50px" data-text="Mail"/>
                     </span>
                     <span class="details">jasskaransingh0007@gmail.com</span>
                 </li>
                 <li>
                     <span className="icon">
-                        <img src="src/assets/images/call.png" alt="or Call Me" width="50px" height="50px" data-text="Call"/>
+                        <img src={assets.Call} alt="or Call Me" width="50px" height="50px" data-text="Call"/>
                     </span>
                     <span class="details">+91 9911318555</span>
                 </li>
                 <li>
                     <span class="icon">
-                        <img src="src/assets/images/location.png" alt="My location" width="50px" height="50px" data-text="Location"/>
+                        <img src={assets.Location} alt="My location" width="50px" height="50px" data-text="Location"/>
                     </span>
                     <span class="details">Chandigarh University</span>
                 </li>
